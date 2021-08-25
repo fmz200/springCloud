cb126e5994371d15d053c05fdb3e1af75429ae8c
feiming <zfm6633@gmail.com>
2021-08-25 23:07:57 +0800

    优化

--- cloudApiService/src/main/java/sun/pojo/RestfulResult.java
+++ cloudApiService/src/main/java/sun/pojo/RestfulResult.java
@@ -1,10 +1,14 @@
 package sun.pojo;
 
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  *
  * @author admin
  */
+@Data
 public class RestfulResult {
 
     private String result = "Success";
@@ -13,34 +17,4 @@ public class RestfulResult {
     private int cntPage;		// page数
     private long cntData;		// 返回数据总数
 
-    public String getResult() {
-        return result;
-    }
-    public void setResult(String result) {
-        this.result = result;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public Object getData() {
-        return data;
-    }
-    public void setData(Object data) {
-        this.data = data;
-    }
-    public int getCntPage() {
-        return cntPage;
-    }
-    public void setCntPage(int cntPage) {
-        this.cntPage = cntPage;
-    }
-    public long getCntData() {
-        return cntData;
-    }
-    public void setCntData(long cntData) {
-        this.cntData = cntData;
-    }
 }
